e5224a00caeac4a7692a384559896166f3701212
SirC00kie <kostomarov.2001@mail.ru>
2022-11-27 15:13:44 +0300

    add mongodb

--- internal/user-service/config/config.go
+++ internal/user-service/config/config.go
@@ -13,6 +13,15 @@ type Config struct {
 		BindIp string `yaml:"bind_ip" env-default:"127.0.0.1"`
 		Port   string `yaml:"port" env-default:"8080"`
 	} `yaml:"listen"`
+	MongoDb struct {
+		Host       string `yaml:"host"`
+		Port       string `yaml:"port"`
+		Database   string `yaml:"database"`
+		AuthDB     string `yaml:"auth_db"`
+		Username   string `yaml:"username"`
+		Password   string `yaml:"password"`
+		Collection string `yaml:"collection"`
+	} `yaml:"mongodb" json:"mongodb"`
 }
 
 var instance *Config
